6c836697ef8a9ad421b31debdfbc2e530b04ce1d
ChihYuChiang <ckccyu@gmail.com>
2018-05-08 13:23:17 -0500

    Add: base antonym processing

--- search.py
+++ search.py
@@ -1,14 +1,54 @@
 import json
 import os
 import sys
+import random
+from collections import Counter
+import nltk
+from nltk.corpus import wordnet as wn
 from python_modules import google
 
 
+#--Function for acquiring lemmas in WordNet structure
+def getAntonyms(words):
+    
+    antonyms = []
+    syns = wn.synsets(word)
+    
+    for syn in syns:
+        antonym = [ant.name() for lemma in syn.lemmas() for ant in lemma.antonyms()]
+        antonyms += antonym
+
+    antonyms_common = [wc[0] for wc in Counter(antonyms).most_common(3)]
+
+    return antonyms_common
+
+[getAntonyms(w) for w in nltk.word_tokenize('fake news')]
+#'best restaurant in chicago'
+#'I love Marry'
+#'Caffeine is good to health'
+#'democratic'
+#'fake news'
+#'real news'
+
+
 def main():
-    keyword = sys.argv[1]
+
+    searchTerm = sys.argv[1]
     session = sys.argv[2]
 
-    results = google.search(keyword, pages=2)
+    searchTerm_token = nltk.word_tokenize(searchTerm)
+    searchTerm_reverse_token = []
+
+    i = 0
+    while i < len(searchTerm_token):
+        t = getAntonyms(searchTerm_token[i])
+        t = random.choice(t) if t else searchTerm_token[i]
+        searchTerm_reverse_token.append(t)
+        i += 1
+
+    searchTerm_reverse = ' '.join(searchTerm_reverse_token)
+
+    results = google.search(searchTerm, pages=1)[0:4] + google.search(searchTerm_reverse, pages=1)[0:4]
 
     '''
     GoogleResult:
