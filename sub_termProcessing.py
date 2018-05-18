b39120f862b6914d9cfcd85ef96f93211a6dd8f8
ChihYuChiang <ckccyu@gmail.com>
2018-05-18 14:32:29 -0700

    Update: stopwords not reversed

--- sub_termProcessing.py
+++ sub_termProcessing.py
@@ -3,7 +3,7 @@ import random
 from collections import Counter
 import nltk
 from nltk.corpus import wordnet as wn
-
+from nltk.corpus import stopwords
 
 #--Acquire lemmas and antonyms of one word in WordNet structure
 def getAntonyms(word):
@@ -19,18 +19,20 @@ def getAntonyms(word):
 
     return antonyms_common
 
-[getAntonyms(w) for w in nltk.word_tokenize('fake news')]
+[getAntonyms(w) for w in nltk.word_tokenize('good')]
 
 
 #--Acquire the reverse terms based on the antonyms of each word
+#Stopwords is not reversed
 def getReverseTerms(searchTerm):
 
     searchTerm_token = nltk.word_tokenize(searchTerm)
+    searchTerm_token_rs = [w if w not in stopwords.words('english') else '' for w in searchTerm_token]
     searchTerm_reverse_token = []
 
     i = 0
-    while i < len(searchTerm_token):
-        t = getAntonyms(searchTerm_token[i])
+    while i < len(searchTerm_token_rs):
+        t = getAntonyms(searchTerm_token_rs[i])
         t = random.choice(t) if t else searchTerm_token[i]
         searchTerm_reverse_token.append(t)
         i += 1
@@ -38,6 +40,8 @@ def getReverseTerms(searchTerm):
     searchTerm_reverse = ' '.join(searchTerm_reverse_token)
 
     return(searchTerm_reverse)
+
+getReverseTerms('this is bad')
 
 
 #--Main process
