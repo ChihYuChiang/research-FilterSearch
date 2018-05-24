68d3966e13211f989956a7ab7a58ede25e4b5c47
ChihYuChiang <ckccyu@gmail.com>
2018-05-24 14:49:39 -0700

    Add: loading spinner

--- public/client.js
+++ public/client.js
@@ -3,8 +3,13 @@
 document.getElementById("form").onsubmit = hideResult;
 
 function hideResult() {
-    var result = document.getElementsByClassName("result")
+    var result = document.getElementsByClassName("result");
     for (var i = 0; i < result.length; i++) {
         result[i].classList.add("hide");
     }
+
+    var loadSpinner = document.createElement("IMG");
+    loadSpinner.src = "/image/loading-spinner.gif";
+    loadSpinner.classList.add("loading-spinner");
+    document.body.appendChild(loadSpinner);
 }
