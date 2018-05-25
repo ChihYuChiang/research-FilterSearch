e1e974b58f9d6da4539de91dceb593a90a3a363b
ChihYuChiang <ckccyu@gmail.com>
2018-05-24 17:36:52 -0700

    Add: submit disable

--- public/client.js
+++ public/client.js
@@ -1,13 +1,18 @@
-
-
+//--Loading spinner
+//When submit form
 document.getElementById("form").onsubmit = hideResult;
 
 function hideResult() {
+    //Disable input
+    document.getElementById("btn-submit").setAttribute("disabled", "disabled");
+
+    //Hide result
     var result = document.getElementsByClassName("result");
     for (var i = 0; i < result.length; i++) {
         result[i].classList.add("hide");
     }
 
+    //Show spinner
     var loadSpinner = document.createElement("IMG");
     loadSpinner.src = "/image/loading-spinner.gif";
     loadSpinner.classList.add("loading-spinner");
